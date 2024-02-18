03b63169345c641b1b74b7529fa9d8c68a857a34
Andrei <132501558+LilAndy2@users.noreply.github.com>
2024-02-18 16:01:31 +0200

    fronted part of wordboard is done

--- Main.java
+++ Main.java
@@ -22,14 +22,30 @@ public class Main {
     }
     public static void startGame(GamePage gamePage, String difficulty) {
         KeyboardPanel keyboard = new KeyboardPanel();
-        keyboard.setLayout(new GridLayout(4, 10, 10, 10));
+        //keyboard.setLayout(new GridLayout(4, 10, 10, 10));
         keyboard.setBackground(new Color(18,18,18,255));
         keyboard.setBounds(250, 700, 500, 200);
         keyboard.setVisible(true);
 
         gamePage.getFrame().getContentPane().removeAll();
         gamePage.getFrame().add(keyboard);
+        gamePage.getFrame().revalidate();
+        gamePage.getFrame().repaint();
 
+        int wordLength = 0;
+
+        switch (difficulty) {
+            case "easy" -> wordLength = 4;
+            case "medium" -> wordLength = 5;
+            case "hard" -> wordLength = 6;
+        }
+
+        WordboardPanel wordboard = new WordboardPanel(wordLength);
+        wordboard.setBackground(new Color(18,18,18,255));
+        wordboard.setBounds(250, 100, 500, 500);
+        wordboard.setVisible(true);
+
+        gamePage.getFrame().add(wordboard);
         gamePage.getFrame().revalidate();
         gamePage.getFrame().repaint();
     }
--- Panel.java
+++ Panel.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.util.ArrayList;
 
-public class KeyboardPanel extends JPanel {
+public interface Panel {
+}
+
+class KeyboardPanel extends JPanel implements Panel {
     private final ArrayList<JButton> buttons;
     public KeyboardPanel() {
         this.buttons = new ArrayList<>();
@@ -12,33 +16,68 @@ public class KeyboardPanel extends JPanel {
             MyButton button = new MyButton(String.valueOf(c), 0, 0, 0, 0, new Font("Times New Roman",
                     Font.BOLD, 20), new Color(255,255,255), new Color(0,0,0));
             button.setSize(100,100);
+
             JPanel panel = new JPanel();
             panel.setBackground(new Color(18,18,18,255));
             panel.add(button, BorderLayout.CENTER);
             panel.setVisible(true);
+
             this.add(panel);
             this.buttons.add(button);
         }
 
         MyButton enterButton = new MyButton("Enter", 0, 0, 0, 0, new Font("Times New Roman",
                 Font.BOLD, 20), new Color(255,255,255), new Color(0,0,0));
+
         JPanel enterPanel = new JPanel();
         enterPanel.setVisible(true);
         enterPanel.setBackground(new Color(18,18,18,255));
         enterPanel.add(enterButton, BorderLayout.CENTER);
+
         this.add(enterPanel);
         this.buttons.add(enterButton);
 
         MyButton deleteButton = new MyButton("Delete", 0, 0, 0, 0, new Font("Times New Roman",
                 Font.BOLD, 20), new Color(255,255,255), new Color(0,0,0));
+
         JPanel deletePanel = new JPanel();
         deletePanel.setVisible(true);
         deletePanel.setBackground(new Color(18,18,18,255));
         deletePanel.add(deleteButton, BorderLayout.CENTER);
+
         this.add(deletePanel);
         this.buttons.add(deleteButton);
     }
     public ArrayList<JButton> getButtons() {
         return this.buttons;
+    }
+}
+
+class WordboardPanel extends JPanel implements Panel {
+    private final JLabel[][] labels;
+    public WordboardPanel(int wordLength) {
+        labels = new JLabel[wordLength][wordLength];
+        this.setLayout(new GridLayout(wordLength, wordLength, 10, 10));
+        for (int i = 0; i < wordLength; i++) {
+            for (int j = 0; j < wordLength; j++) {
+                Border border = BorderFactory.createLineBorder(Color.LIGHT_GRAY, 3);
+                MyLabel label = new MyLabel(null, "", JLabel.CENTER, JLabel.CENTER, new Color(255, 255, 255),
+                        new Font("Times New Roman", Font.PLAIN, 20), 0, new Color(18, 18, 18, 255), JLabel.CENTER,
+                        JLabel.CENTER, 0, 0, 0, 0);
+                //label.setBorder(border);
+
+                JPanel panel = new JPanel();
+                panel.setBackground(new Color(18,18,18,255));
+                panel.add(label, BorderLayout.CENTER);
+                panel.setBorder(border);
+                panel.setVisible(true);
+
+                this.add(panel);
+                labels[i][j] = label;
+            }
+        }
+    }
+    public JLabel[][] getLabels() {
+        return this.labels;
     }
 }
